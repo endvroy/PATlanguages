23b10e4f6b2a263f9cf066194c996e3c28a8803f
Roy <endvroy@gmail.com>
2015-12-17 18:07:32 +0800

    added progress bar support; getPage() now reuses session

--- patFutures.py
+++ patFutures.py
@@ -2,44 +2,53 @@ from pat import getPayload
 from collections import Counter
 from concurrent import futures
 import requests, bs4
-from functools import partial
-
-step = 100
+from tqdm import tqdm
 
 
-def getCount(payload, startPage):
-    endPage = startPage + step
+def getPage(session, page):
+    # print('getting page %d...' % page)
+    url = 'http://www.patest.cn/contests/pat-b-practise/submissions?page=%d' % page
+    res = session.get(url)
+
+    try:
+        res.raise_for_status()
+    except requests.HTTPError as exc:
+        if exc.response.status_code == 404:
+            print('page {} encountered 404'.format(page))
+        else:
+            raise
+    else:
+        return res
+
+
+def extractColumns(res):
     counter = Counter()
-    with requests.Session() as s:
-        s.post('http://www.patest.cn/users/sign_in', data=payload)
-
-        for page in range(startPage, endPage):
-            print('getting page %d...' % page)
-            url = 'http://www.patest.cn/contests/pat-b-practise/submissions?page=%d' % page
-            res = s.get(url)
-
-            try:
-                res.raise_for_status()
-            except requests.HTTPError as exc:
-                if exc.response.status_code == 404:
-                    print('page {} encountered 404'.format(page))
-                else:
-                    raise
-
-            soup = bs4.BeautifulSoup(res.text)
-
-            table = soup.select('table')[0]
-            for row in table.find_all('tr')[1:]:
-                cells = row.find_all('td')
-                counter.update([cells[4].text])
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    table = soup.select('table')[0]
+    for row in table.find_all('tr')[1:]:
+        cells = row.find_all('td')
+        counter.update([cells[4].text])
     return counter
+
+
+def getCount(payload, page):
+    return extractColumns(getPage(payload, page))
 
 
 if __name__ == '__main__':
     payload = getPayload()
-    executor = futures.ThreadPoolExecutor(max_workers=50)
-    results = executor.map(partial(getCount, payload), range(1, 1000, step))
-    counter = Counter()
-    for result in results:
-        counter.update(result)
+    executor = futures.ThreadPoolExecutor(max_workers=100)
+    fs = set()
+    with requests.Session() as session:
+        session.post('http://www.patest.cn/users/sign_in', data=payload)
+        for i in range(1, 1001):
+            future = executor.submit(getCount, session, i)
+            fs.add(future)
+        results = futures.as_completed(fs)
+        results = tqdm(results, total=len(fs))
+
+        # results = executor.map(partial(getCount, payload), range(1, 1000, 100))
+        counter = Counter()
+        for future in results:
+            counter.update(future.result())
     print(counter)
